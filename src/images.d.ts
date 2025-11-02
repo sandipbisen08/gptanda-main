432a27f3a3c59be75c2b357d1bc474d4a7f28a6e
sandipbisen08 <sandipbisen08@gmail.com>
2025-11-02 23:21:04 +0530

    added image code

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,21 +1,21 @@
 declare module '*.jpg' {
-  const value: string;
-  export default value;
+    const value: string;
+    export default value;
 }
 declare module '*.jpeg' {
-  const value: string;
-  export default value;
+    const value: string;
+    export default value;
 }
 declare module '*.png' {
-  const value: string;
-  export default value;
+    const value: string;
+    export default value;
 }
 declare module '*.gif' {
-  const value: string;
-  export default value;
+    const value: string;
+    export default value;
 }
 declare module '*.svg' {
-  import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+    import * as React from 'react';
+    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    export default ReactComponent;
 }
