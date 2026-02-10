dd4e0464a78af46b5fa764481fd5483253048290
sandipbisen08 <sandipbisen08@gmail.com>
2026-02-10 10:23:57 +0530

    Fix: Revert to original App.tsx naming convention

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/main.scss';
-import App from './app';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
