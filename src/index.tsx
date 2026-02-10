9f287820071976ca4fc6be4aab9ae6e7d6b71513
sandipbisen08 <sandipbisen08@gmail.com>
2026-02-10 10:21:11 +0530

    Fix: Update TypeScript config for better module resolution

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/main.scss';
-import App from './index.js';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
