bdebac831f28d1602938fdd63a989f6e1c777058
sandipbisen08 <sandipbisen08@gmail.com>
2026-02-10 10:28:48 +0530

    Test: Try simple test app to isolate deployment issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/main.scss';
-const App = require('./App').default;
+const App = require('./test-app').default;
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
---
+++ src/test-app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestApp: React.FC = () => {
+  return (
+    <div>
+      <h1>Test App</h1>
+      <p>If you see this, the deployment is working!</p>
+    </div>
+  );
+};
+
+export default TestApp;
