018e4a517267b90aebf3e51a2cb42bb4ca1e3a2b
sandipbisen08 <sandipbisen08@gmail.com>
2026-02-10 10:27:05 +0530

    Fix: 1

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/main.scss';
-import App from './App';
+const App = require('./App').default;
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
